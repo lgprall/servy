5946fb70bacf6d3768f485038e407d87774d110a
Larry Prall <lprall@mac.com>
2022-03-12 22:16:31 -0500

    Made the utf-8 stuff universal again. Default on Mac is for "Western (ISO Latin 1)", so this fixes it for all browsers anywhere.

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -142,7 +142,7 @@ defmodule Servy.Handler do
     file = @pages_path |> Path.join(target)
     contents = File.read!(file)
                 |> Earmark.as_html!
-    pfix = utf(Regex.match?(~r/iPad/, conv.headers["User-Agent"]))
+    pfix = ~s(<meta charset="UTF-8">\n)
     %{ conv | status: 200, resp_body: pfix <> contents }
   end
 
@@ -167,9 +167,6 @@ defmodule Servy.Handler do
     #{conv.resp_body}
     """
   end
-  
- defp utf(true), do: ~s(<meta charset="UTF-8">\n)
- defp utf(   _), do: ""
- 
+
 end
 
